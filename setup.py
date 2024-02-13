ce6ea3099897de7ff710421c200aba4488c7d33b
ShukuraliProgrammer <shukuralijob@gmail.com>
2024-02-13 22:09:04 +0500

    difs

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
           version='1.0.0',
           description='This package is preparing now, it is not ready to use but after almost 2 or 3 months it will ready to use.',
           long_description= 'Developers who work in Uzbekistan will use this package to integrate Click UZ Payment System to their own fastapi projects.',
+          long_description_content_type='text',
           author='Shukurali Rezamonov',
           author_email='shukuralijob@gmail.com',
           url='https://pypi.org/project/fastapi-click',
